7f724fe77dbcee644a5b95a315f49d602619b113
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-11-05 07:48:14 -0400

    Initial commit

---
+++ Assets/BaseShake.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public abstract class BaseShake : MonoBehaviour
+{
+    [SerializeField] protected float duration = 2.0f;
+    [SerializeField] protected float magnitude = 1.0f;
+    [SerializeField] protected float frequency = 0.2f;
+
+    protected Transform cameraTransform;
+    protected Vector3 originalPosition;
+    protected float elapsedTime = 0f;
+
+    protected virtual void Awake()
+    {
+        cameraTransform = GetComponent<Camera>().transform;
+        originalPosition = cameraTransform.localPosition;
+    }
+
+    public void Shake()
+    {
+        StartCoroutine(ShakeCoroutine());
+    }
+
+    protected IEnumerator ShakeCoroutine()
+    {
+        elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            Vector3 shakeOffset = CalculateShakeOffset(elapsedTime);
+
+            cameraTransform.localPosition = originalPosition + shakeOffset;
+
+            yield return null;
+        }
+        cameraTransform.localPosition = originalPosition;
+    }
+
+    protected virtual Vector3 CalculateShakeOffset(float elapsedTime)
+    {
+        float x = (Mathf.PerlinNoise(elapsedTime * frequency, 0) * magnitude);
+        float y = (Mathf.PerlinNoise(0, elapsedTime * frequency) * magnitude);
+
+        return new Vector3(x, y, 0f);
+    }
+}
